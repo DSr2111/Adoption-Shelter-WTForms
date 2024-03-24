ba5317aee180091010c0a7a0e92b39ba41260266
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-03-24 09:56:32 -0400

    all pets and add pet routes

--- app.py
+++ app.py
@@ -12,3 +12,28 @@ connect_db(app)
 db.create_all()
 
 
+@app.route("/")
+def list_pets():
+    """List all pets up for adoption"""
+
+    pets = Pet.query.all()
+    return render_template("pet_list.html", pets=pets)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
+    """Add pet to agency"""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        data = {k: v for k, v in form.data.items() if k != "csrf_token"}
+        new_pet = Pet(**data)
+        db.session.add(new_pet)
+        db.session.commit()
+        flash(f"{new_pet.name} added.")
+        return redirect(url_for('list_pets'))
+
+    else:
+        # re-present form for editing
+        return render_template("pet_add_form.html", form=form)
