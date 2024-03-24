ba2a437bda6a736c62a50a9fd8de3c5a0001dd91
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-03-24 09:45:22 -0400

    config and importing from models

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import flask
 from flask_debugtoolbar import DebugToolBarExtension
-from models import db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '12345as'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://adopt"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+
+connect_db(app)
+db.createall()
+
